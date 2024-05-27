3c55fa3065586bc03aaff08cc24c2b98ed97c195
Kymemia <kimemiachristopherr@gmail.com>
2024-05-28 01:26:01 +0300

    pycodestyle fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,6 +8,9 @@ import glob
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 modules = glob.glob(dirname(__file__) + "/*.py")
-
-for module in [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]:
+module_files = (
+        basename(f)[:-3] for f in modules
+        if isfile(f) and not f.endswith('__init__.py')
+        )
+for module in module_files:
     exec(f"from. {module} import *")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views('/status', methods=['GET'])
 def get_status:
     """returns a status OK"""
